feb540c569d440ad823058a198f329fb10f9b45d
Yahya Wessam <yahyawessam2002@gmail.com>
2021-04-04 23:50:29 +0300

    40 px ver

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = class freenitrolegitnovirus extends Plugin {
             let i = 0;
             args[1].content.split(" ").forEach(element => {
                 if (/\<(a|)\:[a-z]*\:[0-9]*\>/gi.test(element)) {
-                    args[1].content = args[1].content.replace(element, args[1].invalidEmojis[i].url + "&size=64")
+                    args[1].content = args[1].content.replace(element, args[1].invalidEmojis[i].url + "&size=40")
                     i += 1;
                 }
             })
@@ -27,7 +27,7 @@ module.exports = class freenitrolegitnovirus extends Plugin {
                     if (args[0].emojiDescriptors[element.props.children.props.columnIndex].isDisabled == true) {
 		                patch("test", MessageEvents, "sendMessage", (msgargs) => {
                             console.log(args[0].emojiDescriptors[element.props.children.props.columnIndex])
-                            msgargs[1].content = msgargs[1].content + " " + args[0].emojiDescriptors[element.props.children.props.columnIndex].emoji.url + "&size=64 "
+                            msgargs[1].content = msgargs[1].content + " " + args[0].emojiDescriptors[element.props.children.props.columnIndex].emoji.url + "&size=40 "
                             unpatch("test")
                         })
                     } else {
