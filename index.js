feb3ae6e4bfef6b8e8ab7047827be9ac779caccc
MoltenCoreDev <craxell.tv@gmail.com>
2021-04-04 20:19:25 +0200

    Ok added progress

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const MessageEvents = getModule("sendMessage")
 
 module.exports = class freenitrolegitnovirus extends Plugin {
     async start() {
+
+
 		patch("emojisend", MessageEvents, "sendMessage", (args) => {
             let i = 0;
             args[1].content.split(" ").forEach(element => {
                 if (/\<(a|)\:[a-z]*\:[0-9]*\>/gi.test(element)) {
-                    console.log(element)
                     args[1].content = args[1].content.replace(element, args[1].invalidEmojis[i].url + "&size=64")
                     i += 1;
                 }
@@ -18,14 +19,17 @@ module.exports = class freenitrolegitnovirus extends Plugin {
         })
 
         const emojipickerrow = getModule(m => m.default?.displayName === "EmojiPickerListRow")
-        patch("test", emojipickerrow, "default", (args, res) => {
+        patch("emojipicker", emojipickerrow, "default", (args, res) => {
             res.props.children.forEach(element => {
                 let cachedFunction = element.props.onClick
-                // console.log(element.props.children.props.columnIndex)
                 
                 element.props.onClick = function(r) {
                     if (args[0].emojiDescriptors[element.props.children.props.columnIndex].isDisabled == true) {
-
+		                patch("test", MessageEvents, "sendMessage", (msgargs) => {
+                            console.log(args[0].emojiDescriptors[element.props.children.props.columnIndex])
+                            msgargs[1].content = msgargs[1].content + " " + args[0].emojiDescriptors[element.props.children.props.columnIndex].emoji.url + "&size=64 "
+                            unpatch("test")
+                        })
                     } else {
                         cachedFunction(r)
                     }
@@ -37,6 +41,6 @@ module.exports = class freenitrolegitnovirus extends Plugin {
 
     stop() {
         unpatch("emojisend")
-        unpatch("test")
+        unpatch("emojipicker")
     }
 };
